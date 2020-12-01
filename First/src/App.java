1108688e51fdd5ee63528bb4ea2ed9501a5db78d
Brhymer <leiqien28@hotmail.com>
2020-11-30 19:57:26 -0800

    moves Scanner input to App level

--- First/src/App.java
+++ First/src/App.java
@@ -1,9 +1,20 @@
-
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        RandomNum num = new RandomNum();
-        System.out.println(num);
+
+        System.out.println("Enter the minimum");
+        Scanner min = new Scanner(System.in);
+        int minInt = min.nextInt();
+        System.out.println("Enter the maximum");
+        Scanner max = new Scanner(System.in);
+        int maxInt = max.nextInt();
+
+        // RandomNum num = new RandomNum();
+        RandomNum.main(minInt, maxInt);
+        // System.out.println(num);
+        min.close();
+        max.close();
     }
 }
 
--- First/src/RandomNum.java
+++ First/src/RandomNum.java
@@ -1,21 +1,13 @@
 import java.util.Random;
-import java.util.Scanner;
 
 public class RandomNum {
-    public static void main(String[] args) {
+    public static int main(int minInt, int maxInt) {
 
         Random output = new Random();
-        System.out.println("Enter the minimum");
-        Scanner min = new Scanner(System.in);
-        int minInt = min.nextInt();
-        System.out.println("Enter the maximum");
-        Scanner max = new Scanner(System.in);
-        int maxInt = max.nextInt();
         int adjMax = maxInt+1-minInt;
-        int x = output.nextInt(adjMax);
-        System.out.println("The number is: " + (x+minInt) );
+        int x = output.nextInt(adjMax) + minInt;
+        System.out.println("The number is: " + (x) );
 
-        min.close();
-        max.close();
+        return x;
     }
 }
